bdf7f515b48b79aec848b5f38a8eef07e15585d1
Bernáth Máté <bernath.mate.new@gmail.com>
2018-09-07 15:47:19 +0200

    The WithdrawalTransactionException class is added to the Exception package.

---
+++ src/main/java/com/supercharge/SuperchargeTest/Exception/WithdrawalTransactionException.java
@@ -0,0 +1,6 @@
+package com.supercharge.SuperchargeTest.Exception;
+
+public class WithdrawalTransactionException extends Throwable {
+    public WithdrawalTransactionException(String s) {
+    }
+}
